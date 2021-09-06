812bf0aef060317ce8bcf9c6aae337e1dc8d5673
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-05 23:11:32 -0500

    added user routes

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,35 @@
+const express = require('express')
+const router = new express.Router()
+const User = require('../models/message')
+const { ensureCorrectUser, ensureLoggedIn } = require('../middleware/auth')
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-
+router.get('/', ensureLoggedIn, async (req, res, next) => {
+  try {
+    const users = await User.all()
+    return res.json({ users })
+  } catch (err) {
+    return next(err)
+  }
+})
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
+router.get('/:username', ensureCorrectUser, async (req, res, next) => {
+  try {
+    const { username } = req.params
+    const user = await User.get(username)
+    return res.json({ user })
+  } catch (err) {
+    return next(err)
+  }
+})
 
 /** GET /:username/to - get messages to user
  *
@@ -21,7 +40,15 @@
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-
+router.get('/:username/to', ensureCorrectUser, async (req, res, next) => {
+  try {
+    const { username } = req.body
+    const messages = await User.messagesTo(username)
+    return res.json({ messages })
+  } catch (err) {
+    return next(err)
+  }
+})
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +59,12 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get('/:username/from', ensureCorrectUser, async (req, res, next) => {
+  try {
+    const { username } = req.body
+    const messages = await User.messagesFrom(username)
+    return res.json({ messages })
+  } catch (err) {
+    return next(err)
+  }
+})
